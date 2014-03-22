857049117b43a8eb2bf3a9f8e876f1303599b082
Wiliam Souza <wiliamsouza83@gmail.com>
2014-03-22 00:20:26 -0300

    Added authenticate controller

--- app/scripts/controllers/users_authenticate_controller.js
+++ app/scripts/controllers/users_authenticate_controller.js
@@ -1,3 +1,20 @@
 ApolloWeb.UsersAuthenticateController = Ember.ObjectController.extend({
-    // Implement your controller here.
+  needs: 'application',
+  email: '',
+  password: '',
+  remember: false,
+  message: '',
+  actions: {
+    authenticate: function() {
+      var self = this;
+      var auth = self.getProperties('email', 'password');
+      Ember.$.post('http://localhost:8000/users/authenticate', JSON.stringify(auth), 'json').then(function(data, textStatus, jqXHR) {
+        self.transitionToRoute('index');
+        localStorage.token = data.token;
+        self.get('controllers.application').set('signedIn', true);
+      }, function(jqXHR, textStatus, errorThrown) {
+        self.setProperties({message: jqXHR.responseText});
+      });
+    }
+  }
 });
